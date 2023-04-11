80e191c19c50d1971e698554e19adfb549c969af
fycn <fannychen623@gmail.com>
2023-04-11 12:22:43 -0400

    Updates

---
+++ precache-manifest.fcf22eb3f621d0f9846a39248dc344c3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "16aa7891b051a4d9e3e5",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/css/main.ef4a0cc6.chunk.css"
+  },
+  {
+    "revision": "16aa7891b051a4d9e3e5",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/js/main.72838f42.chunk.js"
+  },
+  {
+    "revision": "ebcaaf73dfa435e1fbe2",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/js/runtime~main.046fc68d.js"
+  },
+  {
+    "revision": "9cfb7a44b3185a176c8d",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/css/2.f9d5d974.chunk.css"
+  },
+  {
+    "revision": "9cfb7a44b3185a176c8d",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/js/2.b85d6ae1.chunk.js"
+  },
+  {
+    "revision": "acfbd5e5db90b6cc7acaa3097f5c1be5",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/Avatar.acfbd5e5.JPG"
+  },
+  {
+    "revision": "be726be252616aaf4389e8fef847d9cc",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/Resume.be726be2.pdf"
+  },
+  {
+    "revision": "1f337438533c74ce5788a5e84ca51fd8",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/codingquiz.1f337438.png"
+  },
+  {
+    "revision": "666dfdde15634223e2f2561943f5043c",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/ecommercebackend.666dfdde.png"
+  },
+  {
+    "revision": "e7895918ec36f152d6198eddcd383442",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/employeetracker.e7895918.png"
+  },
+  {
+    "revision": "d4f58f5d2be5b5907fb303229786e22b",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/gifmemarvel.d4f58f5d.png"
+  },
+  {
+    "revision": "087f3ac4ffe271f8dd0736c77c6628e8",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/notetaker.087f3ac4.png"
+  },
+  {
+    "revision": "e7baf6cacbefee06a8a583fec667e8a1",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/prework.e7baf6ca.png"
+  },
+  {
+    "revision": "cb09f420ca352923a421e5ec628039fc",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/readmefilegenerator.cb09f420.png"
+  },
+  {
+    "revision": "be996b0e90380253512eb62aed60a34e",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/shareabyte.be996b0e.png"
+  },
+  {
+    "revision": "88ba4b59fcd3020151fa3fa4df49fb98",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/teamprofilegenerator.88ba4b59.png"
+  },
+  {
+    "revision": "d9e23cd595dd0dd26233c93a5c35aeb4",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/weatherdashboard.d9e23cd5.png"
+  },
+  {
+    "revision": "7317f6a69eb9568b952e90e6d8e4b012",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/workdayscheduler.7317f6a6.png"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Fanny-Chen-Digital-Portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "293b50cba9170b1b6bb3df6df693e652",
+    "url": "/Fanny-Chen-Digital-Portfolio/index.html"
+  }
+];
